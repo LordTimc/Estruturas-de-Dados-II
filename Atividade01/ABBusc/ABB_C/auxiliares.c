7462cbf3642d1f7f53b6f19c5cf49fe689442019
Rogers-Rocha <rogerscarvalho13@hotmail.com>
2026-04-10 17:40:31 -0300

    modificado a função leitura_de_string
    deixando a leitura da função mais intuitiva, mesmo já havendo comentários

--- Atividade01/ABBusc/ABB_C/auxiliares.c
+++ Atividade01/ABBusc/ABB_C/auxiliares.c
@@ -14,23 +14,23 @@ char *leitura_de_string(){
     if(fgets(buffer, sizeof(buffer), stdin) != NULL){
         //então remova o \n do final da string e coloque \0
         buffer[strcspn(buffer, "\n")] = 0; // o 0 (zero) aqui é o mesmo que \0
-        int i = 0;
+        int inicio_string = 0;
 
         // buffer[i] significa que enquanto nao chegou no fim da string E
-        // o caractere é espaço ou tab, incremente i
-        while (buffer[i] && (buffer[i] == ' ' || buffer[i] == '\t'))
-            i++;
+        // o caractere é espaço ou tab, incremente i (ou seja, o inicio da string)
+        while (buffer[inicio_string] && (buffer[inicio_string] == ' ' || buffer[inicio_string] == '\t'))
+            inicio_string++;
 
         // se o buffer[i] não estiver no final da string
-        if(buffer[i] != '\0'){
+        if(buffer[inicio_string] != '\0'){
             // j aponta para o ultimo caractere valido da string
-            int j = strlen(buffer) - 1;
+            int fim_string = strlen(buffer) - 1;
             // enquanto j não chegar ao inicio útil da string E o caractere for espaço OU tab, decremente j (ou seja, volta uma posiçao)
-            while(j >= i && (buffer[j] == ' ' || buffer[j] == '\t'))
-                j--;
+            while(fim_string >= inicio_string && (buffer[fim_string] == ' ' || buffer[fim_string] == '\t'))
+                fim_string--;
             // coloca o fim da string (\0) logo após o último caractere válido 
-            buffer[j + 1] = '\0';
-            string = strdup(buffer + i); // ignora os espaço do começo da string
+            buffer[fim_string + 1] = '\0';
+            string = strdup(buffer + inicio_string); // ignora os espaço do começo da string
             // depois strdup a aloca memoria
             // depois string copia string limpa
         }
